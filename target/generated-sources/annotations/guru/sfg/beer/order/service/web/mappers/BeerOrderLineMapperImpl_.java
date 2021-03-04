6c2860b580af38de84693b6c366d48de36516896
Frida YAN <hyan6@jaguarlandrover.com>
2021-03-04 15:33:31 +0800

    local mysql config

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl_.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl_.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-04T13:12:11+0800",
+    date = "2021-03-04T13:25:27+0800",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11 (Oracle Corporation)"
 )
 @Component
